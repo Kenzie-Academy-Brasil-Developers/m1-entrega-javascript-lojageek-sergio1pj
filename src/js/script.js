3bb264c27a7842df415d0af9bc5463e4a2f24843
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-11 08:08:54 -0300

    Modificando funções createActionItem e createPaitingItem

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,26 @@
-/*
+
 let item = {
 
     }
 let listFigure = [];
 let listPainting = [];
-function createActionItem(objeto){
-
+function createActionItem(imagem, nome, valor){
+    let item = {
+        "imagem": imagem,
+        "nome": nome,
+        "valor": valor
+    }
+    listFigure.push(item);
+    return `Boneco de ação ${item["name"]} foi adicionado com sucesso`;
 };
 function createPaintingItem(objeto){
-
+    let item = {
+        "imagem": imagem,
+        "nome": nome,
+        "valor": valor
+    }
+    listPainting.push(item);
+    return `Quadro decorativo ${item["name"]} foi adicionado com sucesso`;
 }    
 for(let i = 0; i < listFigure.length; i++){
     //lógica
@@ -18,8 +30,8 @@ let nome = document.createElement('span')
 nome.innerText = `${listPerson[i].nome}`
 elementoLista.appendChild(nome)
 secaolistFigure.appendChild(elementoLista)
-*/
-alert("12");
+
+
 
 
 
