6de4b44212a23747aa93931c1ab9a5e264d5a8dc
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-11 08:18:14 -0300

    Executando createPaitingItem x3

--- src/js/script.js
+++ src/js/script.js
@@ -9,11 +9,11 @@ function createActionItem(imagem, nome, valor){
     listFigure.push(item);
     return `Boneco de ação ${item["name"]} foi adicionado com sucesso`;
 };
-createActionItem("assets\img\actions\animewoman.jpg", "anime woman", "R$ 100")
-createActionItem("assets\img\actions\dragonballpersonagem.jpg", "dragon ball personagem", "R$ 100")
-createActionItem("assets\img\actions\starwarspersonagem.jpg", "star war spersonagem", "R$ 100")
-console.log(listFigure)
-function createPaintingItem(objeto){
+createActionItem("assets\img\actions\animewoman.jpg", "anime woman", "R$ 99");
+createActionItem("assets\img\actions\dragonballpersonagem.jpg", "dragon ball personagem", "R$ 99");
+createActionItem("assets\img\actions\starwarspersonagem.jpg", "star war spersonagem", "R$ 99");
+
+function createPaintingItem(imagem, nome, valor){
     let item = {
         "imagem": imagem,
         "nome": nome,
@@ -21,7 +21,10 @@ function createPaintingItem(objeto){
     }
     listPainting.push(item);
     return `Quadro decorativo ${item["name"]} foi adicionado com sucesso`;
-}    
+};
+createPaintingItem("assets\img\painting\clock.jpg", "clock", "R$ 199");    
+createPaintingItem("assets\img\painting\gamepad.jpg", "gamepad", "R$ 199");
+createPaintingItem("assets\img\painting\personagem.jpg", "personagem", "R$ 199");     
 /*
 let elementoLista = document.createElement('li')
 let nome = document.createElement('span')
