77df75e432dee53d02379cdafc3057909d8b71fb
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-11 18:11:07 -0300

    reorganizando o js

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,7 @@
-let listFigure = [];
 let listPainting = [];
-function createActionItem(imagem, nome, valor){
-    let item = {
-        "imagem": imagem,
-        "nome": nome,
-        "valor": valor
-    }
-    listFigure.push(item);
-    return `Boneco de ação ${item["name"]} foi adicionado com sucesso`;
-};
-createActionItem("assets/img/actions/animewoman.jpg", "Anime woman", "R$ 99");
-createActionItem("assets/img/actions/dragonballpersonagem.jpg", "Dragon ball personagem", "R$ 99");
-createActionItem("assets/img/actions/starwarspersonagem.jpg", "Star wars personagem", "R$ 99");
-
+let listFigure = [];
+let secaoPainting = document.querySelector(".paintings")
+let secaoFigure = document.querySelector(".action_figures")
 function createPaintingItem(imagem, nome, valor){
     let item = {
         "imagem": imagem,
@@ -22,9 +11,15 @@ function createPaintingItem(imagem, nome, valor){
     listPainting.push(item);
     return `Quadro decorativo ${item["name"]} foi adicionado com sucesso`;
 };
-createPaintingItem("assets/img/painting/clock.jpg", "Clock", "R$ 199");    
-createPaintingItem("assets/img/painting/gamepad.jpg", "Gamepad", "R$ 199");
-createPaintingItem("assets/img/painting/personagem.jpg", "Personagem", "R$ 199");
+function createActionItem(imagem, nome, valor){
+    let item = {
+        "imagem": imagem,
+        "nome": nome,
+        "valor": valor
+    }
+    listFigure.push(item);
+    return `Boneco de ação ${item["name"]} foi adicionado com sucesso`;
+};
 function createCard(produto){
     let tagLi = document.createElement("li");
     tagLi.classList.add("product-card");
@@ -48,7 +43,11 @@ function createCard(produto){
 function createSection(list, section) {
     list.forEach(element => section.appendChild(createCard(element)));
 }
-let secaoFigure = document.querySelector(".action_figures")
-let secaoPainting = document.querySelector(".paintings")
+createPaintingItem("assets/img/painting/clock.jpg", "Clock", "R$ 199");    
+createPaintingItem("assets/img/painting/gamepad.jpg", "Gamepad", "R$ 199");
+createPaintingItem("assets/img/painting/personagem.jpg", "Personagem", "R$ 199");
+createActionItem("assets/img/actions/animewoman.jpg", "Anime woman", "R$ 99");
+createActionItem("assets/img/actions/dragonballpersonagem.jpg", "Dragon ball personagem", "R$ 99");
+createActionItem("assets/img/actions/starwarspersonagem.jpg", "Star wars personagem", "R$ 99");
 createSection(listFigure, secaoFigure);
 createSection(listPainting, secaoPainting);
